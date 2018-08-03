9ea7295f947bc140d8ee0db8f11cf8123f98a974
suanny fabyne <suanny_fabyne@hotmail.com>
2018-08-03 19:08:33 -0300

    Falta display

---
+++ build/default/debug/Vazio.o.d
@@ -0,0 +1,4 @@
+build/default/debug/Vazio.o:  \
+	/home/suannyfabyne/MPLABXProjects/atividade3.X/Vazio.ASM  \
+	/opt/microchip/mplabx/v4.10/mpasmx/p12f675.inc  \
+
---
+++ build/default/production/Vazio.o.d
@@ -0,0 +1,4 @@
+build/default/production/Vazio.o:  \
+	/home/suannyfabyne/MPLABXProjects/atividade3.X/Vazio.ASM  \
+	/opt/microchip/mplabx/v4.10/mpasmx/p12f675.inc  \
+
